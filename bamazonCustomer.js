832a533e78f735112365a4eaa5c49a6b6e613b77
lisacampbell <59990057+lisaswindler@users.noreply.github.com>
2020-05-12 00:21:37 -0600

    customer file started

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,7 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
-// var command = process.argv[2];
-// var input = process.argv.slice(3).join(" ");
+
 var connection = mysql.createConnection({
 	host: "localhost",
 	port: 8889,
@@ -9,16 +8,51 @@ var connection = mysql.createConnection({
 	password: "root",
 	database: "bamazon"
 });
+
 function listProducts(){
-	//SELECT to view all authors
 	connection.query("SELECT * FROM products", function(err, res){
         if(err) throw err;
-        console.log("what");
-        // for (var i=0; i<res.length; i++) {
-        //     console.log("\nItem #" + res[i].item_id +
-        //         "\nItem: " + res[i].product_name +
-        //         "\nPrice: " + res[i].price);
-        // }
-	});
+        console.log(res);
+        for (var i=0; i<res.length; i++) {
+            console.log("\nItem #" + res[i].item_id +
+                "\nItem: " + res[i].product_name +
+                "\nPrice: " + res[i].price);
+        }
+        inquirer.prompt([
+                {
+                type: "input",
+                message: "Enter the ID of the product you would like to buy.",
+                name: "id"
+                },
+                {
+                type: "input",
+                message: "Enter the purchase quantity.",
+                name: "quantity"
+                }
+                ])
+                .then(function(answer) {
+                console.log("\nYou have chosen item #" + answer.id + ": " + res[answer.id - (parseInt(1))].product_name);
+                console.log("Quantity in cart: " + answer.quantity);
+                });
+                // prompt.next({
+                //         type: "confirm",
+                //         message: "Make purchase?",
+                //         name: "purchase",
+                //         default: true
+                // })
+                //         .then(function(answer) {
+                //                 if (answer.confirm) {
+                //                 console.log("Thank you for your purchase!");
+                //                 } else {
+                //                 console.log("No purchase has been made.");
+                //                 }
+                //         });
+        });
 }
-listProducts();
+
+connection.connect(function(err) {
+	if(err) throw err;
+        listProducts();
+        // promptToBuy();
+        connection.end();
+});
